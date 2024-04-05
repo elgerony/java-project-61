ed4ed2bf4bca1c15855e1f463aeb9f2711e8770f
Vlada <koschelevavlada@gmail.com>
2024-04-06 00:07:14 +0300

    added Progression class and updated App class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import hexlet.code.games.Calc;
 import hexlet.code.games.Even;
 import hexlet.code.games.GCD;
+import hexlet.code.games.Progression;
 
 import java.util.Scanner;
 
@@ -13,6 +14,7 @@ public class App {
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
         System.out.println("4 - GCD");
+        System.out.println("5 - Progression");
         System.out.println("0 - Exit");
 
         Scanner choice = new Scanner(System.in);
@@ -30,6 +32,9 @@ public class App {
         }
         if (choiceNum == 4) {
             GCD.GCDGame();
+        }
+        if (choiceNum == 5) {
+            Progression.progressionGame();
         }
 
 
--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -1,2 +1,49 @@
-package hexlet.code.games;public class Progression {
+package hexlet.code.games;
+
+import hexlet.code.Engine;
+
+import java.util.Random;
+
+public class Progression {
+    public static void progressionGame() {
+        String chosenGame = "What number is missing in the progression?";
+        String[][] rounds = new String[3][2];
+        for (int i = 0; i < rounds.length; i++) {
+            rounds[i] = randomQuestion();
+        }
+        Engine.gameProcess(chosenGame, rounds);
+    }
+    public static String[] randomQuestion() {
+        int minInt = 5;
+        int maxInt = 30;
+        var temp = randomTemp();
+        var startInt = new Random().nextInt(maxInt - minInt) + minInt;
+        var firstInt = startInt;
+        var numbers = new String[10];
+        numbers[0] = String.valueOf(firstInt);
+        for (int i = 1; i < numbers.length; i++ ) {
+            numbers[i] = String.valueOf(firstInt + temp);
+            firstInt = Integer.valueOf(numbers[i]);
+        }
+        var randomPlace = randomPlace();
+        numbers[randomPlace] = "..";
+        String progression = "";
+        for (int i = 0; i < numbers.length; i++ ) {
+            progression += numbers[i] + " ";
+        }
+        var trimedProgression = progression.trim();
+        var correctAnswer = String.valueOf(startInt + temp * randomPlace);
+        var question = trimedProgression + ".";
+        return new String[]{question, correctAnswer};
+    }
+    public static int randomTemp() {
+        int minTemp = 1;
+        int maxTemp = 5;
+        return new Random().nextInt(maxTemp - minTemp) + minTemp;
+    }
+    public static int randomPlace() {
+        int min = 0;
+        int max = 9;
+        return new Random().nextInt(max - min) + min;
+    }
 }
